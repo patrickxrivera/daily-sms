deabcdce063949d3ecd2d1a7bca4b9f10ff0ed14
Patrick Rivera <pxr13@my.fsu.edu>
2018-09-01 15:42:27 -0700

    Improved error handling for registration

--- src/api/user.js
+++ src/api/user.js
@@ -4,23 +4,19 @@ import { REGISTER_USER_ENDPOINT, VERIFY_USER_ENDPOINT } from 'utils/endpoints';
 
 import * as h from './helpers';
 
+const handleError = ({ response }) =>
+  !response || !response.data.message
+    ? { error: true, message: 'Unknown error. Please try again.' }
+    : { error: true, ...response.data };
+
 export const registerUser = (userData) =>
   axios
     .post(REGISTER_USER_ENDPOINT, userData)
     .then(({ data }) => data)
-    // .catch(({ response }) => ({ error: true, ...response.data }));
-    .catch((err) => {
-      console.dir(err);
-      return { error: true, message: 'Unknown error. Please try again.' };
-    });
-
-const handleVerififyUserError = ({ response }) =>
-  !response || !response.data.message
-    ? { error: true, message: 'Unknown error. Please try again.' }
-    : { error: true, ...response.data };
+    .catch(handleError);
 
 export const verifyUser = (verificationCode, userId) =>
   axios
     .post(`${VERIFY_USER_ENDPOINT}/${userId}`, verificationCode)
     .then(({ data }) => data)
-    .catch(handleVerififyUserError);
+    .catch(handleError);
