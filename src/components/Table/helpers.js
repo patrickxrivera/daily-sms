c2e2c2cc0f6521e5d9f39d0af9faafc831b82358
Patrick Rivera <pxr13@my.fsu.edu>
2018-09-06 15:24:41 -0700

    Fixed formatTime helper

--- src/components/Table/helpers.js
+++ src/components/Table/helpers.js
@@ -16,9 +16,12 @@ const getHour = (time) => Number(time.slice(0, 2));
 export const formatTime = (time) => {
   const hour = getHour(time);
 
-  if (isAM(hour)) return time[0] === '0' ? `${time.slice(1)} AM` : `${time} AM`;
+  if (isAM(hour)) {
+    if (hour === '00') return `12${time.slice(2)} AM`;
+    return time[0] === '0' ? `${time.slice(1)} AM` : `${time} AM`;
+  }
 
   const newHour = hour % 12;
 
-  return `${newHour}:${time.slice(3)} PM`;
+  return `${newHour === 0 ? 12 : newHour}:${time.slice(3)} PM`;
 };
